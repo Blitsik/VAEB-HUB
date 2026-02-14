88b6ee68b4cca799dd40cc37ee439281cb75ce0f
Blitsik <blitsoljko@yandex.ru>
2026-02-14 20:11:20 +0500

    tsunami.lua

--- tsunami.lua
+++ tsunami.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://monlua-protector.vercel.app/raw/87cb241f07ac297ec1efcbb1ed06839a"))()
+getgenv().SCRIPT_KEY="Acess to vaeb hub!-7da05824-975e-4c0e-895f-9c7185a2832e";
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4a9c8b6fa6a3020592429cc422ccc35b86ce480249cc0bf3ee375d9b00a51991/download"))()
