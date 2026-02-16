8c1c1accc0222fc623b02dd7684c1708972c1f42
Blitsik <blitsoljko@yandex.ru>
2026-02-16 20:42:02 +0500

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local Games = {
-    [131623223084840] = "https://raw.githubusercontent.com/Blitsik/VAEB-HUB/refs/heads/main/sab.lua", -- Steal a brainrot
-    [109983668079237] = "https://raw.githubusercontent.com/Blitsik/VAEB-HUB/refs/heads/main/gm.lua", -- Escape Tsunami for brainrots
+    [109983668079237] = "https://raw.githubusercontent.com/Blitsik/VAEB-HUB/refs/heads/main/sab.lua", -- Steal a brainrot
+    [131623223084840] = "https://raw.githubusercontent.com/Blitsik/VAEB-HUB/refs/heads/main/gm.lua", -- Escape Tsunami for brainrots
     [119987266683883] = "https://pastebin.com/raw/0s3aMs9K" -- Survive LAVA for brainrots
 }
 
