9b2cecae5aaef117e4374d74cb1160afe12930e1
Blitsik <blitsoljko@yandex.ru>
2026-02-16 22:06:50 +0500

    loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,7 @@ local Games = {
     [109983668079237] = "https://raw.githubusercontent.com/Blitsik/VAEB-HUB/refs/heads/main/sab.lua", -- Steal a brainrot
     [131623223084840] = "https://raw.githubusercontent.com/Blitsik/VAEB-HUB/refs/heads/main/gm.lua", -- Escape Tsunami for brainrots
     [119987266683883] = "https://pastebin.com/raw/0s3aMs9K" -- Survive LAVA for brainrots
+    [17625359962] = "https://pastebin.com/raw/rLXvC39M" -- rivals
 }
 
 local URL = Games[game.PlaceId]
